76fcf418fa4e53b524bb41e07bb14d8bab9178e9
qandoni <yunev_i@mail.ru>
2026-04-07 12:49:34 +0400

    feature/statistics

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -13,6 +13,9 @@ import (
 	core_pgx_pool "github.com/qandoni/golangToDoApp/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/qandoni/golangToDoApp/internal/core/transport/http/middleware"
 	core_http_server "github.com/qandoni/golangToDoApp/internal/core/transport/http/server"
+	statistics_postgres_repository "github.com/qandoni/golangToDoApp/internal/features/statistics/repository/postgres"
+	statistics_service "github.com/qandoni/golangToDoApp/internal/features/statistics/service"
+	statistics_transport_http "github.com/qandoni/golangToDoApp/internal/features/statistics/transport/http"
 	task_postgres_repository "github.com/qandoni/golangToDoApp/internal/features/tasks/repository/postgres"
 	tasks_service "github.com/qandoni/golangToDoApp/internal/features/tasks/service"
 	tasks_transport_http "github.com/qandoni/golangToDoApp/internal/features/tasks/transport/http"
@@ -62,6 +65,11 @@ func main() {
 	tasksService := tasks_service.NewTasksService(tasksRepository)
 	tasksTransportHTTP := tasks_transport_http.NewTasksHTTPHandler(tasksService)
 
+	logger.Debug("initialining feature", zap.String("feature", "statistics"))
+	statisticsRepository := statistics_postgres_repository.NewStatisticsRepository(pool)
+	statisticsService := statistics_service.NewStatisticsService(statisticsRepository)
+	statisticsTransportHTTP := statistics_transport_http.NewStatisticsHTTPHandler(statisticsService)
+
 	logger.Debug("initializing HTTP server")
 	httpServer := core_http_server.NewHTTPServer(
 		core_http_server.NewConfigMust(),
@@ -74,6 +82,7 @@ func main() {
 	apiVersionRouterV1 := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
 	apiVersionRouterV1.RegisterRoutes(usersTransportHTTP.Routes()...)
 	apiVersionRouterV1.RegisterRoutes(tasksTransportHTTP.Routes()...)
+	apiVersionRouterV1.RegisterRoutes(statisticsTransportHTTP.Routes()...)
 
 	//Example of using middleware on APIVersionRouter
 	// apiVersionRouterV2 := core_http_server.NewAPIVersionRouter(
